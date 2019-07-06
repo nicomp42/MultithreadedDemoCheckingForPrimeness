836cbd869dbc258d42622d51f6c34df8ec695cdd
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-07-06 16:33:35 -0400

    Tuning performance

--- MultithreadedDemoCheckingForPrimeness/src/multithreadedDemoCheckingForPrimeness/Main.java
+++ MultithreadedDemoCheckingForPrimeness/src/multithreadedDemoCheckingForPrimeness/Main.java
@@ -15,13 +15,13 @@ public class Main {
 	}
 
 	public static void demo() {
-		System.out.println("This machine has " + getNumberOfCores() + " cores.");
 
-		final long largest64BitPrime = 2764787846358431193L;
+//		final long aPrimeNumber = 2764787846358431197L;
+		final long aPrimeNumber = 5915587277L;
 		long startTime = System.currentTimeMillis();
 		Boolean thisPartIsPrime = true;
-		for (long i = 3; i <= largest64BitPrime; i += 2) {
-			if (largest64BitPrime % i == 0) {
+		for (long i = 3; i <= aPrimeNumber; i += 2) {
+			if (aPrimeNumber % i == 0) {
 				thisPartIsPrime = false;	// We found a divisor. The number is not prime. Give up. Sigh.
 				break;
 			}			
@@ -30,14 +30,19 @@ public class Main {
 		System.out.println("Result = " + thisPartIsPrime + ", Single threaded: Total execution time: " + (endTime - startTime) + " milliseconds.");
 
 		// Try it with however many cores are available.
+		System.out.println("This machine has " + getNumberOfCores() + " cores.");
 		ArrayList<PrimessThread> primenessThreads = new ArrayList<PrimessThread>();
 		int numberOfCores = getNumberOfCores() - 2;
-		long increment = largest64BitPrime / numberOfCores;
+		long limit = aPrimeNumber / 2;
+		long increment = limit / numberOfCores;
+		long start, end;
+		start = 3;
+		end = start + increment;
 		for (int i = 0; i < numberOfCores; i++) {
-			long start, end;
-			start = i * increment;
-			end = start + increment;
-			primenessThreads.add(new PrimessThread(largest64BitPrime, start, end));
+			primenessThreads.add(new PrimessThread(i + 1, aPrimeNumber, start, end));
+			start = end + 1;
+			end += increment;
+			if (end > limit) {end = limit; }
 		}
 		// Launch all the threads to run in parallel
 		startTime = System.currentTimeMillis();
--- MultithreadedDemoCheckingForPrimeness/src/multithreadedDemoCheckingForPrimeness/PrimessThread.java
+++ MultithreadedDemoCheckingForPrimeness/src/multithreadedDemoCheckingForPrimeness/PrimessThread.java
@@ -8,16 +8,19 @@ public class PrimessThread extends Thread {
 
 	private long num, start, end;
 	private Boolean thisPartIsPrime;
+	private int threadNumber;
 
-	public PrimessThread(long num, long start, long end) {
+	public PrimessThread(int threadNumber, long num, long start, long end) {
 		this.start = start;
 		this.end = end;
 		this.num = num;
 		thisPartIsPrime = false;
+		this.threadNumber = threadNumber;
 	}
 
 	@Override
 	public void run() {
+		System.out.println("Thread Number " + threadNumber + ": Starting thread from " + start + " to " + end);
 		thisPartIsPrime = true;	// Assume the best
 		for (long i = start; i <= end; i += 2) {
 			if (num % start == 0) {
@@ -25,7 +28,7 @@ public class PrimessThread extends Thread {
 				break;
 			}
 		}
-		System.out.println("Result = " + thisPartIsPrime + ", number = " + num + ", thread counted from " + start + " to " + end);
+		System.out.println("Thread Number " + threadNumber + ": Result = " + thisPartIsPrime + ", number = " + num + ", thread counted from " + start + " to " + end);
 	}
 	/***
 	 * Get the result of what the thread did
